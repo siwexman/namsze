4a0081c4849e413e7a30f032b548dc41685327b8
Karol <karol.straczek@gmail.com>
2026-02-25 18:48:28 +0100

    CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import churchRouter from './routes/churchRoute';
 import massRouter from './routes/massRoute';
 import confessionRouter from './routes/confessionRoute';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+app.options('*', cors());
 
 // ROUTES
 app.use('/api/v1/churches', churchRouter);
